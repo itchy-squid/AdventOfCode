0fa8add5ab61d5ded33f495f7f845011690d63ef
Amanda Santi <amanda.santi@hcss.com>
2022-12-14 12:31:19 -0600

    One of these days I will learn to read

--- 2022/12/Service.cs
+++ 2022/12/Service.cs
@@ -9,52 +9,47 @@ namespace Advent22.Day11
     {
         public int Solve(string[] lines)
         {
-            var width = lines[0].Length;
-            var length = lines.Length;
-            var graph = new Graph<Elevation>(width, length);
+            var wid = lines[0].Length;
+            var len = lines.Length;
+            var graph = new Graph<Elevation>(wid, len);
             var start = (Node<Elevation>?)null;
             var end = (Node<Elevation>?)null;
 
-            foreach (var y in Enumerable.Range(0, length))
-                foreach (var x in Enumerable.Range(0, width))
+            foreach (var y in Enumerable.Range(0, len))
+                foreach (var x in Enumerable.Range(0, wid))
                 {
                     graph.AddNode(x, y, Deserialize(lines[y][x]));
                     start = (graph[x, y].Value.IsStart) ? graph[x, y] : start;
                     end = (graph[x, y].Value.IsEnd) ? graph[x, y] : end;
-
-                    if (y > 0) TryAddEdge(graph, graph[x, y - 1], graph[x, y]);
-                    if (x > 0) TryAddEdge(graph, graph[x - 1, y], graph[x, y]);
                 }
 
+            foreach (var y in Enumerable.Range(0, len))
+                foreach (var x in Enumerable.Range(0, wid))
+                {
+                    if (x > 0) TryAddEdge(graph[x, y], graph[x - 1, y]);
+                    if (y > 0) TryAddEdge(graph[x, y], graph[x, y - 1]);
+                    if (y < len - 1) TryAddEdge(graph[x, y], graph[x, y + 1]);
+                    if (x < wid - 1) TryAddEdge(graph[x, y], graph[x + 1, y]);
+                }
 
-            Console.WriteLine($"width: {width}");
-            foreach (var v in graph.First(n => n.Id == 3464).Neighbors)
-            {
-                Console.WriteLine($"neighbor at {v.Id}");
-            }
-            // Print(graph, (_, _, n) => n.Value.IsStart);
-
-            var path = graph.ShortestPath(start!, end!).ToList();
-
-            Console.WriteLine(path.Select(n => n.Id.ToString()).Aggregate((a, b) => a != null ? (b != null ? $"{a} {b}" : $"{a}") : $"{b}"));
-
-            Print(graph, n => path.Any(pn => n.Id == pn.Id));
-
-            return path.Count - 1;
+            var (dist, path) = graph.ShortestPath(start!, end!);
+            return dist;
         }
 
-        private void TryAddEdge(Graph<Elevation> g, Node<Elevation> a, Node<Elevation> b)
+        private void TryAddEdge(Node<Elevation> a, Node<Elevation> b)
         {
-            if (a.Value.IsReachable(b.Value)) g.AddEdge(a, b);
+            if (b.Value.Height - a.Value.Height <= 1) a.Neighbors.Add(b);
         }
 
         private static Elevation Deserialize(char c)
         {
+            var heightFrom = (char c) => c - 'a' + 1;
+
             switch (c)
             {
-                case 'S': return new Elevation { Height = 1, IsStart = true, IsEnd = false };
-                case 'E': return new Elevation { Height = 26, IsStart = false, IsEnd = true };
-                default: return new Elevation { Height = c - 'a' + 1, IsStart = false, IsEnd = false };
+                case 'S': return new Elevation { Height = heightFrom('a'), IsStart = true, IsEnd = false };
+                case 'E': return new Elevation { Height = heightFrom('z'), IsStart = false, IsEnd = true };
+                default: return new Elevation { Height = heightFrom(c), IsStart = false, IsEnd = false };
             }
         }
 
@@ -80,6 +75,11 @@ namespace Advent22.Day11
                 }
                 Console.WriteLine();
             }
+        }
+
+        private int ToId(int x, int y, int width)
+        {
+            return y * width + x;
         }
     }
 }
