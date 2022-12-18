b44a35c0cd7fe0a92c896d10d1f7a982e394e1f8
Amanda Santi <amanda.santi@hcss.com>
2022-12-18 15:09:15 -0600

    Day 15, problem 1

---
+++ 2022/15/Model.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Text;
+
+namespace _15
+{
+
+    public record Range
+    {
+        public Range(int min, int max)
+        {
+            this.Min = min;
+            this.Max = max;
+        }
+
+        public int Min { get; init; }
+        public int Max { get; init; }
+        public int Size => Max - Min + 1;
+
+        public bool Intersects(int n) => Min <= n && n <= Max;
+    }
+
+    public class RangeCollection : IEnumerable<Range>
+    {
+        private List<Range> _ranges = new List<Range>();
+
+        public void Add(int min, int max)
+        {
+            // Find the first place where we can start mucking around.
+            var startIdx = _ranges.FindIndex(r => r.Min >= min || r.Max >= min);
+            var endIdx = _ranges.FindLastIndex(r => r.Min <= max || r.Max <= max);
+
+            var rangeStart = startIdx < 0 ? min : Math.Min(_ranges[startIdx].Min, min);
+            var rangeEnd = endIdx < 0 ? max : Math.Max(_ranges[endIdx].Max, max);
+            var newRange = new Range(rangeStart, rangeEnd);
+
+            if (startIdx < 0) _ranges.Add(newRange);
+            else
+            {
+                for (var i = endIdx; i >= startIdx; i--) _ranges.RemoveAt(i);
+                _ranges.Insert(startIdx, newRange);
+            }
+        }
+
+        public IEnumerator<Range> GetEnumerator() => _ranges.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_ranges).GetEnumerator();
+    }
+
+    public static class RangeExtensions
+    {
+        public static string Dump(this IEnumerable<Range> ranges)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var range in ranges)
+            {
+                builder = builder.Append($"[{range.Min},{range.Max}] ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ 2022/15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+using _15;
+
+var LINE = 2000000;
+var FILE = "input.txt";
+
+var calculateDistance = (int x1, int y1, int x2, int y2) => Math.Abs(x2 - x1) + Math.Abs(y2 - y1);
+
+var lines = await File.ReadAllLinesAsync(FILE);
+var ranges = new RangeCollection();
+var beacons = new List<int>();
+
+var pattern = @"-?\d+";
+
+foreach (var line in lines)
+{
+    var pt = Regex.Matches(line, pattern).Where(m => m.Success).Select(m => Int32.Parse(m.Value)).ToArray();
+    var dist = calculateDistance(pt[0], pt[1], pt[2], pt[3]);
+    Console.WriteLine($"s:({pt[0]},{pt[1]})  \tb:({pt[2]},{pt[3]})  \td:{dist}");
+
+    var delta = dist - Math.Abs(pt[1] - LINE);
+
+    if (delta >= 0)
+    {
+        Console.WriteLine($"Adding range [{pt[0] - delta},{pt[0] + delta}]");
+        ranges.Add(pt[0] - delta, pt[0] + delta);
+        Console.WriteLine(ranges.Dump());
+        Console.WriteLine();
+    }
+
+    if (pt[3] == LINE) beacons.Add(pt[2]);
+}
+
+var size = 0;
+foreach (var range in ranges)
+{
+    size += range.Size;
+}
+
+foreach (var beacon in beacons.Distinct())
+{
+    foreach (var range in ranges)
+    {
+        if (range.Intersects(beacon)) size -= 1;
+    }
+}
+
+Console.WriteLine(ranges.Dump());
+Console.WriteLine(size);
+
+
+
