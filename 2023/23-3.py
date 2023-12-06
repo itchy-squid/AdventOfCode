18485717e0756bfc69bd6a0589be79e5c925c6c9
Amanda S <amanda.santi@live.com>
2023-12-05 18:30:09 -0600

    23-4-2 complete

--- 2023/23-3.py
+++
@@ -1,9 +0,0 @@
-class formats:
-    PURPLE = '\033[95m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
-    GREEN = '\033[92m'
-    ORANGE = '\033[93m'
-    RED = '\033[91m'
-    WHITE = '\033[0m'
-    BOLD = '\033[1m'
